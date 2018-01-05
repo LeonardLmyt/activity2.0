7e82922a32021a15f87d77a35fb494739b0bd366
lmyt <1730481311@qq.com>
2018-01-05 11:19:05 +0800

    Initialization

---
+++ pages/detatl/play_all.js
@@ -0,0 +1,67 @@
+// pages/detatl/play_all.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    bigTitle:"雪色浪漫 滑雪 看冰河雾松 ",
+    time:'06/18至06/22'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,14 @@
+let mixin = require('../../public/js/mixin.js'),
+mx = null,
+app = getApp();
+
+Page({
+  data: {
+    place_name:'广州',
+    select_activities:'精选活动',
+    set_out:'广州出发',
+    tite:'免费挑战人生的第一哥500米,四姑娘山大峰登山',
+    content:'背包玩世界彩虹之国,自驾南非克鲁格国家公园',
+    di:'上海'
+  }
+})
